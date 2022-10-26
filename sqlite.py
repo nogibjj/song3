9351bccbbd8fe19e41bbdbd34389f61011ad8897
SongYoungOh <SongYoungOh@MacBook-Pro-8.local>
2022-10-25 22:08:28 -0400

    added files

--- sqlite.py
+++ sqlite.py
@@ -8,4 +8,4 @@ nowDatetime = now.strftime('%Y-%m-%d %H:%M:%S')
 print('nowDatetime : ', nowDatetime)
 
 print('sqlite3.version : ', sqlite3.version)
-print('sqlite3.sqite-version : ', sqlite3.sqlite_version)
+print('sqlite3.sqlite-version : ', sqlite3.sqlite_version)
