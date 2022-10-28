8a10ff2665591e6c2be8c3e62c7a42797e831cd1
songyoungoh <112578065+songyoungoh@users.noreply.github.com>
2022-10-28 04:10:17 +0000

    modified sqlite_db file

--- sqlite_db.py
+++ sqlite_db.py
@@ -5,7 +5,7 @@ conn = sqlite3.connect("sample.db")
 cur = conn.cursor()
 
 # create table
-table = cur.execute(""" CREATE TABLE test(
+table = cur.execute(""" CREATE TABLE member(
     id INT PRIMARY KEY,
     name CHAR(25),
     salary CHAR(25),
@@ -15,16 +15,21 @@ table = cur.execute(""" CREATE TABLE test(
 print("\n [+] The table has been created Successfully ")
 
 # insert data
-cur.execute("INSERT INTO test (id, name, salary, joining_date) VALUES (1001, 'David', 50000, '1-08-2019')")
-cur.execute("INSERT INTO test (id, name, salary, joining_date) VALUES (1002, 'Sally', 80000, '3-09-2020')")
-cur.execute("INSERT INTO test (id, name, salary, joining_date) VALUES (1003, 'Lucy', 90000, '8-08-2020')")
-cur.execute("INSERT INTO test (id, name, salary, joining_date) VALUES (1004, 'Chris', 100000, '9-09-2020')")
-cur.execute("INSERT INTO test (id, name, salary, joining_date) VALUES (1005, 'Bob', 111000, '10-05-2019')")
+cur.execute("INSERT INTO member (id, name, salary, joining_date) VALUES (1001, 'David', 50000, '1-08-2019')")
+cur.execute("INSERT INTO member (id, name, salary, joining_date) VALUES (1002, 'Sally', 80000, '3-09-2020')")
+cur.execute("INSERT INTO member (id, name, salary, joining_date) VALUES (1003, 'Lucy', 90000, '8-08-2020')")
+cur.execute("INSERT INTO member (id, name, salary, joining_date) VALUES (1004, 'Chris', 100000, '9-09-2020')")
+cur.execute("INSERT INTO member (id, name, salary, joining_date) VALUES (1005, 'Bob', 111000, '10-05-2019')")
 print("\n [+] The Data has been inserted Successfully ")
 
 # query the table
-cur.execute("SELECT id,name FROM test")
+cur.execute("SELECT * FROM member ORDER BY name")
 table = cur.fetchall()
+print("\n [+] Querying the data \n")
 for i in table:
     print(i)
 print("\n [+] Successfully connected to the database")
+
+cur.close()
+conn.commit()
+conn.close()
