ea27fe470e966539eca636569103ec98b329bf3a
songyoungoh <112578065+songyoungoh@users.noreply.github.com>
2022-11-02 01:36:26 +0000

    modified file name

--- test_db.py
+++ test_db.py
@@ -1,35 +1,32 @@
-import sqlite3
+import sqlite3 as sql
 
 # set up connection to the database
-conn = sqlite3.connect("sample.db")
+conn = sql.connect("test.db")
 cur = conn.cursor()
 
 # create table
-table = cur.execute(""" CREATE TABLE member(
+table = cur.execute(""" CREATE TABLE employee(
     id INT PRIMARY KEY,
     name CHAR(25),
     salary CHAR(25),
     joining_date DATE
 );
 """)
-print("\n [+] The table has been created Successfully ")
 
 # insert data
-cur.execute("INSERT INTO member (id, name, salary, joining_date) VALUES (1001, 'David', 50000, '1-08-2019')")
-cur.execute("INSERT INTO member (id, name, salary, joining_date) VALUES (1002, 'Sally', 80000, '3-09-2020')")
-cur.execute("INSERT INTO member (id, name, salary, joining_date) VALUES (1003, 'Lucy', 90000, '8-08-2020')")
-cur.execute("INSERT INTO member (id, name, salary, joining_date) VALUES (1004, 'Chris', 100000, '9-09-2020')")
-cur.execute("INSERT INTO member (id, name, salary, joining_date) VALUES (1005, 'Bob', 111000, '10-05-2019')")
-print("\n [+] The Data has been inserted Successfully ")
+cur.execute("INSERT INTO employee (id, name, salary, joining_date) VALUES (1001, 'David', 50000, '1-08-2019')")
+cur.execute("INSERT INTO employee (id, name, salary, joining_date) VALUES (1002, 'Sally', 80000, '3-09-2020')")
+cur.execute("INSERT INTO employee (id, name, salary, joining_date) VALUES (1003, 'Lucy', 90000, '8-08-2020')")
+cur.execute("INSERT INTO employee (id, name, salary, joining_date) VALUES (1004, 'Chris', 100000, '9-09-2020')")
+cur.execute("INSERT INTO employee (id, name, salary, joining_date) VALUES (1005, 'Bob', 111000, '10-05-2019')")
 
 # query the table
-cur.execute("SELECT * FROM member ORDER BY name")
+cur.execute("SELECT * FROM employee ORDER BY name")
 table = cur.fetchall()
 print("\n [+] Querying the data \n")
+
 for i in table:
     print(i)
 print("\n [+] Successfully connected to the database")
 
-cur.close()
 conn.commit()
-conn.close()
