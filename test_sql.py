6e1824a1f3b0c390361bda9bc5478fbd4748edb7
songyoungoh <112578065+songyoungoh@users.noreply.github.com>
2022-11-09 02:49:23 -0500

    Update test_sql.py

--- test_sql.py
+++ test_sql.py
@@ -3,7 +3,3 @@ import sqlite3 as sql
 # set up connection to the database
 conn = sql.connect("test.db")
 cur = conn.cursor()
-
-# check sqlite version
-print("sqlite3.version : ", sql.version)
-print("sqlite3.sqlite-version : ", sql.sqlite_version)
