e27f551d681742552e4c1639e50dc2d1707aba6f
songyoungoh <112578065+songyoungoh@users.noreply.github.com>
2022-11-09 03:31:49 -0500

    Update test_sql.py

--- test_sql.py
+++ test_sql.py
@@ -3,8 +3,7 @@ import sqlite3 as sql
 # set up connection to the database
 conn = sql.connect("baseball.sqlite")
 cur = conn.cursor()
-
-cur.execute("SELECT * FROM pitching WHERE playerID == 'ryuhy01'")
-rows = cur.fetchall()
-for row in rows:
-    print(row)
+def test():
+    cur.execute("SELECT * FROM pitching WHERE playerID == 'ryuhy01'")
+    rows = cur.fetchall()
+    assert rows[0][0] == 42452
