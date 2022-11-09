cf597f47d208d03c8737bce91cacff69e5f6e62f
songyoungoh <112578065+songyoungoh@users.noreply.github.com>
2022-11-09 03:25:38 -0500

    Update test_sql.py

--- test_sql.py
+++ test_sql.py
@@ -1,5 +1,10 @@
 import sqlite3 as sql
 
 # set up connection to the database
-conn = sql.connect("test.db")
+conn = sql.connect("baseball.sqlite")
 cur = conn.cursor()
+
+cur.execute("SELECT * FROM pitching WHERE playerID == 'ryuhy01'")
+rows = cur.fetchall()
+for row in rows:
+    print(row)
